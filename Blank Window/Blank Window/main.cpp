39e679d90ff8c32fd66a8bd01c5558c83f3d6002
YiqiaoLiu <yiqiao.liu@duke.edu>
2016-08-26 16:41:59 -0400

    Setting the enviroment and create a brand new blank window

---
+++ Blank Window/Blank Window/main.cpp
@@ -0,0 +1,79 @@
+﻿#include<Windows.h>
+
+
+// Callback function declaration
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+// The main function of the blank window
+int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nShowCmd) {
+
+	// Avoid un-using warning
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+
+	// Define a Window Class and set the variable value
+	WNDCLASSEX wndClass = { 0 };
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = WndProc;
+	wndClass.hInstance = hInstance;
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)(GetStockObject(BLACK_BRUSH));
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = "Window";
+
+	// Register the window
+	if (!RegisterClassEx(&wndClass)) {
+		MessageBeep(0);
+		return -1;
+	}
+
+	// Define the width and length of the window
+	RECT rc = { 0, 0, 640, 480 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+	
+	// Create the window
+	HWND hwnd = CreateWindowA("Window", "BlankWindow", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
+	if (!hwnd) {
+		MessageBeep(0);
+		return -1;
+	}
+
+	// Show the window
+	ShowWindow(hwnd, nShowCmd);
+
+	// Get the message event
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT) {
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			// Update the window
+		}
+	}
+
+	return msg.wParam;
+}
+
+// The implementation of Callback Function
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	PAINTSTRUCT paintStruct;
+	HDC hDC;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hDC = BeginPaint(hwnd, &paintStruct);
+		EndPaint(hwnd, &paintStruct);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+	return 0;
+}
