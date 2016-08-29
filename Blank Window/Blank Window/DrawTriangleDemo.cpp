f99896a2a669aacdf431a1ee7c9f853d14eb7d95
YiqiaoLiu <yiqiao.liu@duke.edu>
2016-08-29 00:01:30 -0400

    Implement the unload function in DrawTriangleDemo class

--- Blank Window/Blank Window/DrawTriangleDemo.cpp
+++ Blank Window/Blank Window/DrawTriangleDemo.cpp
@@ -8,7 +8,20 @@ bool DrawTriangleDemo::LoadContent() {
 	return true;
 }
 
-void DrawTriangleDemo::UnloadContent() {}
+void DrawTriangleDemo::UnloadContent() {
+
+	// Release the component
+	if (triangleVertexShader_) triangleVertexShader_->Release();
+	if (trianglePixelShader_) trianglePixelShader_->Release();
+	if (triangleInputLayout_) triangleInputLayout_->Release();
+	if (triangleBuffer_) triangleBuffer_->Release();
+	
+	// Reset all the pointer variable
+	triangleVertexShader_ = nullptr;
+	trianglePixelShader_ = nullptr;
+	triangleInputLayout_ = nullptr;
+	triangleBuffer_ = nullptr;
+}
 
 void DrawTriangleDemo::Update(float DeltaTime) {}
 
